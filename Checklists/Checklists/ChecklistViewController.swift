e2455f48a992d5439eb214234642a90292e98490
Alyssa Ma <Alyssa.Ma80@myhunter.cuny.edu>
2021-04-07 20:50:28 -0400

    fixed crash bug

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -26,8 +26,25 @@ class ChecklistViewController: UITableViewController {
         _ tableView: UITableView,
         cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem",
-                                                 for: indexPath)
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "ChecklistItem",
+            for: indexPath)
+        
+        //finding the view with tag 1000
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        //index path is an object that points to a specific row in the table
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row == 4 {
+            label.text = "Eat ice cream"
+        }
         return cell
     }
 }
