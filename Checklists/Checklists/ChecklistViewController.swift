4e17b780ae307bc90ede987e10f1944b5a37ba4d
Alyssa Ma <Alyssa.Ma80@myhunter.cuny.edu>
2021-04-07 22:19:33 -0400

    add and remove checkmark

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -15,11 +15,30 @@ class ChecklistViewController: UITableViewController {
     }
     
     // MARK: - Table View Data Source
+    override func tableView
+    (
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    )
+        {
+            if let cell = tableView.cellForRow(at: indexPath)
+            {
+                if cell.accessoryType == .none
+                {
+                    cell.accessoryType = .checkmark
+                }
+                else
+                {
+                    cell.accessoryType = .none
+                }
+            }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     override func tableView(
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        return 5
+        return 100
     }
 
     override func tableView(
@@ -34,15 +53,15 @@ class ChecklistViewController: UITableViewController {
         let label = cell.viewWithTag(1000) as! UILabel
         
         //index path is an object that points to a specific row in the table
-        if indexPath.row == 0 {
+        if indexPath.row % 5 == 0 {
             label.text = "Walk the dog"
-        } else if indexPath.row == 1 {
+        } else if indexPath.row % 5 == 1 {
             label.text = "Brush my teeth"
-        } else if indexPath.row == 2 {
+        } else if indexPath.row % 5 == 2 {
             label.text = "Learn iOS development"
-        } else if indexPath.row == 3 {
+        } else if indexPath.row % 5 == 3 {
             label.text = "Soccer practice"
-        } else if indexPath.row == 4 {
+        } else if indexPath.row % 5 == 4 {
             label.text = "Eat ice cream"
         }
         return cell
